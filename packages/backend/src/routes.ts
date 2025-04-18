83df07157dc6de481c78dc99534e9e1b8b917202
iamiks <rmstjr1030@naver.com>
2025-04-19 02:27:13 +0900

    Fix : tsoa 관련 파일 생성 문제 해결

---
+++ packages/backend/src/routes.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+import { Express } from "express";
+
+export function RegisterRoutes(app: Express) {
+  // placeholder for routes
+}
